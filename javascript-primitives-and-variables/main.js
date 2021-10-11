ecaf073d0536d22e4a1d03d41e6e903f4bb9712a
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-11 10:47:15 -0700

    Finished Javascript primitives and variables assignment

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+
+var fullName = 'Dre Sean';
+var isCool = false;
+var totalPets = 1;
+var unknown = null;
+var never =
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('value of unknown:', unknown);
+console.log('value of never:', never);
+
+console.log('type of', typeof fullName);
+console.log('typeof', typeof isCool);
+console.log('typeof', typeof totalPets);
+console.log('typeof', typeof unknown);
+console.log('typeof', typeof never);
