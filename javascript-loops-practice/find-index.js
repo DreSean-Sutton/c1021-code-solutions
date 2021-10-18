808552d5f935a5ded6919936ed6e109458aeea0a
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-18 00:29:24 -0700

    Work in progress

--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -1,1 +1,8 @@
 /* exported findIndex */
+function findIndex(array, value) {
+  var sameNumbers = [];
+  for (var i = 0; i < array.length; i++) {
+    sameNumbers = value.findIndex(array[i]);
+  }
+  return sameNumbers;
+}
