360a734d6c75e7f642774d02a13cafd8c0a2df8c
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-17 20:25:51 -0700

    Work in progress

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -1,1 +1,10 @@
 /* exported filterOutStrings */
+function filterOutStrings(strings) {
+  var noStrings = [];
+  for (var i = 0; i < strings.length; i++) {
+    if (strings[i] === String) {
+      delete strings[i];
+    } else noStrings.push(strings[i]);
+  }
+  return noStrings;
+}
