35a4a4b0e84ae09d82e05ecc0ea6f20905014018
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-18 18:02:46 -0700

    Work in progress

---
+++ dom-manipulation/main.js
@@ -0,0 +1,22 @@
+// var numberOfClicks = [];
+
+// var $hotButton = document.querySelector('.hot-button');
+// var $clickCount = document.querySelector('.click-count');
+// $hotButton.addEventListener('click', clickCount);
+
+// /* PseudoCode:
+// 3 storage place for 3 variables
+// 1 storage place is given the value of .hot-button
+// 2nd storage place is given the value of .click-count
+// need number to start at 0 and each time it's clicked it goes up by 1 up to 25.
+// need number to only go up if it is clicked on.
+// number of clicks must be shown at all times
+
+// */
+// function clickCount(event) {
+//   numberOfClicks.push(event);
+//   console.log('numberOfClicks:', numberOfClicks);
+//   return numberOfClicks;
+// }
+
+// var $clickText = document.querySelector('button.textContent');
