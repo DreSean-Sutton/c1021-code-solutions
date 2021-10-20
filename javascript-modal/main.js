8e5e413bc9e48ce684577cf84790f25ecc254ea8
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-19 21:01:09 -0700

    Work in progress

---
+++ javascript-modal/main.js
@@ -0,0 +1,14 @@
+// var $pageButton = document.querySelector('#page-button');
+// var $modalDisplay = document.querySelector('#modal-display');
+// var $modalButton = document.querySelector('#modal-button');
+// var onMainPage = true;
+// $modalButton.addEventListener('click', clickModalbutton);
+// $pageButton.addEventListener('click', clickMainPageButton);
+
+// function clickModalbutton(event) {
+
+// }
+
+// function clickMainPageButton(event) {
+
+// }
