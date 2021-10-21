4d525b017db24952b7643a127a38fdead6bb4eba
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-21 15:03:31 -0700

    I can tell I'm getting better cuz I instantly fixed the issue and understood why you said it was wrong even tho I was getting the right value LOL

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,9 +2,9 @@
 function filterOutStrings(strings) {
   var noStrings = [];
   for (var i = 0; i < strings.length; i++) {
-    if (typeof (strings[i]) === 'string') {
-      delete strings[i];
-    } else noStrings.push(strings[i]);
+    if (typeof (strings[i]) !== 'string') {
+      noStrings.push(strings[i]);
+    }
   }
   return noStrings;
 }
