2b1de627f48d82a55bf2298982f6933b48473ded
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-20 22:02:49 -0700

    Finished dom-creation

--- dom-creation/main.js
+++ dom-creation/main.js
@@ -1,98 +1,96 @@
-// /* exported pokedex */
+/* exported pokedex */
 
-// var pokedex = [
-//   {
-//     number: '001',
-//     name: 'Bulbasaur',
-//     description: 'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
-//     imageUrl: 'images/bulbasaur.png'
-//   },
-//   {
-//     number: '004',
-//     name: 'Charmander',
-//     description: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
-//     imageUrl: 'images/charmander.png'
-//   },
-//   {
-//     number: '007',
-//     name: 'Squirtle',
-//     description: 'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
-//     imageUrl: 'images/squirtle.png'
-//   },
-//   {
-//     number: '002',
-//     name: 'Ivysaur',
-//     description: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
-//     imageUrl: 'images/ivysaur.png'
-//   },
-//   {
-//     number: '005',
-//     name: 'Charmeleon',
-//     description: 'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
-//     imageUrl: 'images/charmeleon.png'
-//   },
-//   {
-//     number: '008',
-//     name: 'Wartortle',
-//     description: 'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
-//     imageUrl: 'images/wartortle.png'
-//   },
-//   {
-//     number: '003',
-//     name: 'Venusaur',
-//     description: 'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
-//     imageUrl: 'images/venusaur.png'
-//   },
-//   {
-//     number: '006',
-//     name: 'Charizard',
-//     description: 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
-//     imageUrl: 'images/charizard.png'
-//   },
-//   {
-//     number: '009',
-//     name: 'Blastoise',
-//     description: 'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
-//     imageUrl: 'images/blastoise.png'
-//   }
-// ];
+var pokedex = [
+  {
+    number: '001',
+    name: 'Bulbasaur',
+    description: 'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+    imageUrl: 'images/bulbasaur.png'
+  },
+  {
+    number: '004',
+    name: 'Charmander',
+    description: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+    imageUrl: 'images/charmander.png'
+  },
+  {
+    number: '007',
+    name: 'Squirtle',
+    description: 'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
+    imageUrl: 'images/squirtle.png'
+  },
+  {
+    number: '002',
+    name: 'Ivysaur',
+    description: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+    imageUrl: 'images/ivysaur.png'
+  },
+  {
+    number: '005',
+    name: 'Charmeleon',
+    description: 'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
+    imageUrl: 'images/charmeleon.png'
+  },
+  {
+    number: '008',
+    name: 'Wartortle',
+    description: 'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
+    imageUrl: 'images/wartortle.png'
+  },
+  {
+    number: '003',
+    name: 'Venusaur',
+    description: 'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+    imageUrl: 'images/venusaur.png'
+  },
+  {
+    number: '006',
+    name: 'Charizard',
+    description: 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
+    imageUrl: 'images/charizard.png'
+  },
+  {
+    number: '009',
+    name: 'Blastoise',
+    description: 'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
+    imageUrl: 'images/blastoise.png'
+  }
+];
 
-// function renderPokemon(pokemon) {
-//   var rowDiv = document.querySelector('div.row');
+function renderPokemon(pokemon) {
 
-//   var columnDiv = document.createElement('div');
-//   rowDiv.appendChild(columnDiv);
-//   columnDiv.setAttribute('class', 'column-third');
+  var columnDiv = document.createElement('div');
+  rowDiv.appendChild(columnDiv);
+  columnDiv.setAttribute('class', 'column-third');
 
-//   var pokemonCardDiv = document.createElement('div');
-//   columnDiv.appendChild(pokemonCardDiv);
-//   pokemonCardDiv.setAttribute('class', 'pokemon-card');
+  var pokemonCardDiv = document.createElement('div');
+  columnDiv.appendChild(pokemonCardDiv);
+  pokemonCardDiv.setAttribute('class', 'pokemon-card');
 
-//   var pokemonImage = document.createElement('img');
-//   pokemonImage.setAttribute('src', pokemon.imageURL);
-//   pokemonImage.setAttribute('alt', pokemon + ' Picture');
-//   pokemonCardDiv.appendChild(pokemonImage);
+  var pokemonImage = document.createElement('img');
+  pokemonImage.setAttribute('src', pokemon.imageUrl);
+  pokemonImage.setAttribute('alt', pokemon + ' Picture');
+  pokemonCardDiv.appendChild(pokemonImage);
 
-//   var pokemonCardText = document.createElement('div');
-//   pokemonCardText.setAttribute('class', 'pokemon-card-text');
-//   pokemonCardDiv.appendChild(pokemonCardText);
+  var pokemonCardText = document.createElement('div');
+  pokemonCardText.setAttribute('class', 'pokemon-card-text');
+  pokemonCardDiv.appendChild(pokemonCardText);
 
-//   var pokemonH2 = document.createElement('pokemonH2');
-//   pokemonH2.textContent = pokemon.name;
-//   pokemonCardText.appendChild(pokemonH2);
+  var pokemonH2 = document.createElement('pokemonH2');
+  pokemonH2.textContent = pokemon.name;
+  pokemonCardText.appendChild(pokemonH2);
 
-//   var pokemonH3 = document.createElement('pokemonH3');
-//   pokemonH3.textContent = pokemon.number;
-//   pokemonCardText.appendChild(pokemonH3);
+  var pokemonH3 = document.createElement('pokemonH3');
+  pokemonH3.textContent = pokemon.number;
+  pokemonCardText.appendChild(pokemonH3);
 
-//   var pokemonP = document.createElement('pokemonP');
-//   pokemonP.textContent = pokemon.description;
-//   pokemonCardText.appendChild(pokemonP);
-
-//   for (var i = 0; i < pokedex.length; i++) {
-//     var allTogetherNow = [];
-//     allTogetherNow.push(pokedex[i]);
-//   }
-//   rowDiv.appendChild(allTogetherNow);
-//   return allTogetherNow;
-// }
+  var pokemonP = document.createElement('pokemonP');
+  pokemonP.textContent = pokemon.description;
+  pokemonCardText.appendChild(pokemonP);
+}
+var rowDiv = document.querySelector('div.row');
+for (var i = 0; i < pokedex.length; i++) {
+  renderPokemon(pokedex[i]);
+  var allTogetherNow = document.createElement('node');
+  rowDiv.appendChild(allTogetherNow);
+}
