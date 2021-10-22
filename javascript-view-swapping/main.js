4372de9c01b30bc78ed91bc3a336b103e1281d57
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-21 17:24:55 -0700

    Work in progress

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,15 @@
+// var $tabContainer = document.querySelector('.tab-container');
+// var $allTabElements = document.querySelectorAll('.tab');
+// var $allViewElements = document.querySelectorAll('.view');
+
+// $tabContainer.addEventListener('click', clickOnTab);
+
+// function clickOnTab(event) {
+//   if (event.target.matches('.tab') === true) {
+//     console.log('event.targetResult:', event.target);
+//     event.target.className = 'tab active';
+//   } else {
+//     for (var i = 0; i < $allTabElements.length; i++) {
+//     event.target.className = 'tab';
+//   }
+// }
