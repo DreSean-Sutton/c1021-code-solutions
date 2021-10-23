b7994df26f3333ab2cd75cf58df5f299b44cb321
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-23 13:12:03 -0700

    Work in progress

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,28 +1,29 @@
-// var $tabContainer = document.querySelector('.tab-container');
-// var $allTabElements = document.querySelectorAll('.tab');
-// var $allViewElements = document.querySelectorAll('.view');
+var $tabContainer = document.querySelector('.tab-container');
+var $allTabElements = document.querySelectorAll('.tab');
+var $allViewElements = document.querySelectorAll('.view');
 
-// $tabContainer.addEventListener('click', clickOnTab);
+$tabContainer.addEventListener('click', clickOnTab);
 
-// function clickOnTab(event) {
-//   debugger;
-//   if (!event.target.matches('.tab')) {
-//     return;
-//   }
-//   for (var tabIndex = 0; tabIndex < $allTabElements.length; tabIndex++) {
-//     if ($allTabElements[tabIndex] === event.target) {
-//       $allTabElements[tabIndex].className = 'tab active';
-//     } else {
-//       $allTabElements[tabIndex].className = 'tab';
-//     }
-//   }
-//   var $dataView = event.target.getAttribute('data-view');
+function clickOnTab(event) {
+  // debugger;
+  if (!event.target.matches('.tab')) {
+    return;
+  }
+  for (var tabIndex = 0; tabIndex < $allTabElements.length; tabIndex++) {
+    if ($allTabElements[tabIndex] === event.target) {
+      $allTabElements[tabIndex].className = 'tab active';
+    } else {
+      $allTabElements[tabIndex].className = 'tab';
+    }
+  }
+  var $dataView = event.target.getAttribute('data-view');
 
-//   for (var viewIndex = 0; viewIndex < $allViewElements; viewIndex++) {
-//     if ($allViewElements[viewIndex] === $dataView) {
-//       $allViewElements.className = 'data-view';
-//     } else {
-//       $allViewElements[viewIndex].className = 'hidden';
-//     }
-//   }
-// }
+  for (var viewIndex = 0; viewIndex < $allViewElements.length; viewIndex++) {
+    if ($allViewElements[viewIndex] !== $dataView) {
+      $allViewElements.className = 'view hidden';
+    } else {
+      $allViewElements[viewIndex].className = 'view';
+    }
+  }
+  // return $tabContainer;
+}
