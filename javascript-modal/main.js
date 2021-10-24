49df16b8f7a4f196ce5f9f9bc983146bc2c15233
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-23 23:07:29 -0700

    Finished javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,14 +1,23 @@
-// var $pageButton = document.querySelector('#page-button');
-// var $modalDisplay = document.querySelector('#modal-display');
-// var $modalButton = document.querySelector('#modal-button');
-// var onMainPage = true;
-// $modalButton.addEventListener('click', clickModalbutton);
-// $pageButton.addEventListener('click', clickMainPageButton);
+var $pageButton = document.querySelector('#page-button');
+var $displayNone = document.querySelector('#display-none');
+var $modalButton = document.querySelector('#modal-button');
+var $overlay = document.querySelector('#overlay');
+var onMainPage = true;
+$modalButton.addEventListener('click', clickModalbutton);
+$pageButton.addEventListener('click', clickMainPageButton);
 
-// function clickModalbutton(event) {
+function clickMainPageButton(event) {
+  if (onMainPage) {
+    $displayNone.className = '';
+    $overlay.className = 'overlay';
+    onMainPage = false;
+  }
+}
 
-// }
-
-// function clickMainPageButton(event) {
-
-// }
+function clickModalbutton(event) {
+  if (onMainPage === false) {
+    $displayNone.className = 'display-none';
+    $overlay.className = '';
+    onMainPage = true;
+  }
+}
