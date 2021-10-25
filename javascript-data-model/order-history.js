fc4465a5e58b9678c733fe99b5111bb7b05b9cca
DreSean-Sutton <dreseansutton@gmail.com>
2021-10-25 13:00:02 -0700

    Fixed Javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,48 +1,83 @@
 var orderHistory = [
   {
     name: 'Javascript for impatient programmers',
-    orderNumber: '114 - 3941689 - 8772232',
-    orderPlaced: 'August 4, 2020',
-    shipTo: 'JS Masher',
-    deliveryDate: 'Aug 8, 2020',
-    total: 34.00,
     price: 31.55,
     author: 'Rauschmayer, Dr. Axel',
-    returnWindow: 'Sept 7, 2020'
+    items:
+    [
+      {
+        orderPlaced: 'August 4, 2020',
+        shipTo: 'JS Masher',
+        total: 34.00,
+        orderNumber: '114 - 3941689 - 8772232',
+        deliveryDate: 'Aug 8, 2020',
+        returnWindow: 'Sept 7, 2020'
+      }
+    ]
 
   },
   {
     name: 'The Timeless Way of Building',
-    orderPlaced: 'July 19, 2020',
-    orderNumber: '113 - 9984268 - 1280257',
     price: 41.33,
-    shipTo: 'JS Masher',
-    deliveryDate: 'July 20, 2020',
     author: 'Alexander, Christopher',
-    returnWindow: 'Aug 19, 2020',
-
-    total: 44.53
+    items:
+    [
+      {
+        orderPlaced: 'July 19, 2020',
+        shipTo: 'JS Masher',
+        total: 44.53,
+        orderNumber: '113 - 9984268 - 1280257',
+        deliveryDate: 'July 20, 2020',
+        returnWindow: 'Aug 19, 2020'
+      }
+    ]
   },
   {
     name: 'Gamecube Controller Adapter',
-    orderPlaced: 'July 4, 2020',
-    orderNumber: '114 - 2875557 - 9059409',
     price: 15.98,
-    shipTo: 'JS Masher',
-    deliveryDate: 'Jul 7, 2020',
     author: 'none',
-    returnWindow: 'Aug 5, 2020',
-    total: 17.22
+    items:
+    [
+      {
+        orderPlaced: 'July 4, 2020',
+        shipTo: 'JS Masher',
+        total: 17.22,
+        orderNumber: '114 - 2875557 - 9059409',
+        deliveryDate: 'Jul 7, 2020',
+        returnWindow: 'Aug 5, 2020'
+      }
+    ]
   },
   {
     name: 'Gamecube Controller',
-    orderPlaced: 'July 3, 2020',
-    orderNumber: '133 - 2883177 - 2648248',
-    total: 138.93,
-    shipTo: 'JS Masher',
-    deliveryDate: 'July 5, 2020',
-    price: [94.95, 33.99],
+    price: 94.95,
     author: 'none',
-    returnWindow: 'Aug 4, 2020'
+    items:
+    [
+      {
+        orderPlaced: 'July 3, 2020',
+        shipTo: 'JS Masher',
+        total: 138.93,
+        orderNumber: '133 - 2883177 - 2648248',
+        deliveryDate: 'July 5, 2020',
+        returnWindow: 'Aug 4, 2020'
+      }
+    ]
+  },
+  {
+    name: 'The Art of Sql',
+    price: 33.99,
+    author: 'Faroult, Stephane',
+    items:
+    [
+      {
+        orderPlaced: 'July 3, 2020',
+        shipTo: 'JS Masher',
+        total: 138.93,
+        orderNumber: '133 - 2883177 - 2648248',
+        deliverDate: 'July 5, 2020',
+        returnWindow: 'Aug 4, 2020'
+      }
+    ]
   }
 ];
