19fec8c514fca7e5f7cdb27c0496502bb19ef478
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-01 11:50:36 -0700

    Thing acting weird

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,20 @@
+/* exported calculator */
+
+// var calculator = {
+
+//   add: function {
+//     (x, y)
+//     var sum = (x + y);
+//     return sum;
+//   }
+
+//   function(x, y) {
+//     var difference = (x - y)
+//     return difference;
+//   }
+
+//   function(x, y) {
+//     var multiply = (x * y)
+//     return multiply
+//   }
+// }
