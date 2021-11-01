a982621a2695828f99b578f460e837a4d8ea2114
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-01 01:42:39 -0700

    Work in progress

---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,7 @@
+/* exported setValue */
+
+/* return the value of the object at key
+*/
+function setValue(object, key, value) {
+  return object[key].value;
+}
---
+++ object-manipulation-1/src/to-object.js
@@ -0,0 +1,10 @@
+// /* exported toObject */
+
+// function toObject(keyValuePair) {
+//   var firstValue = keyValuePair[0];
+//   var secondValue = keyValuePair[1];
+//   var finalObject = {
+//     firstValue: secondValue
+//   };
+//   return finalObject;
+// }
