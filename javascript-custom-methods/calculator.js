22cc7a8b58997209f7ff4059f9779aab00d8d635
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-01 23:37:33 -0700

    finished javascript-custom-methods

--- javascript-custom-methods/calculator.js
+++ javascript-custom-methods/calculator.js
@@ -1,32 +1,40 @@
-// /* exported calculator */
+/* exported calculator */
 
-// var calculator = {
-//   add: function (x, y) {
-//     var sum = (x + y);
-//     return sum;
-//   },
+var calculator = {
+  add: function (x, y) {
+    var sum = (x + y);
+    return sum;
+  },
 
-//   subtract: function (x, y) {
-//     var difference = (x - y);
-//     return difference;
-//   },
+  subtract: function (x, y) {
+    var difference = (x - y);
+    return difference;
+  },
 
-//   multiply: function (x, y) {
-//     var multiply = (x * y);
-//     return multiply;
-//   },
-//   divide: function (x, y) {
-//     var divide = (x / y);
-//     return divide;
-//   },
-//   square: function (x) {
-//     var square = (x * x);
-//     return square;
-//   },
-//   sumAll: function (numbers) {
-//     for (var i = 0; i < numbers.length; i++) {
-//       var sum += numbers[i];
-//     }
-//     return sum;
-//   }
-// };
+  multiply: function (x, y) {
+    var multiply = (x * y);
+    return multiply;
+  },
+  divide: function (x, y) {
+    var divide = (x / y);
+    return divide;
+  },
+  square: function (x) {
+    var square = (x * x);
+    return square;
+  },
+  sumAll: function (numbers) {
+    var sum = null;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  },
+  getAverage: function (numbers) {
+    var average = null;
+    for (var i = 0; i < numbers.length; i++) {
+      average += numbers[i];
+    }
+    return average / numbers.length;
+  }
+};
