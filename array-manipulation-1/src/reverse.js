38ec5aeac832fb93ce459fcc9d6d2f0639a6179f
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-02 08:49:22 -0700

    Finish array-manipulation-1

--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,5 +1,9 @@
 /* exported reverse */
-
+/* get a place to store information and name it newArr
+Take the length of the array - 1 and assign that information to a new storage named i. the loop through until i equals 0
+push the index of array to newArr and increment i down by 1
+return newArr
+*/
 function reverse(array) {
   var newArr = [];
   for (var i = array.length - 1; i >= 0; i--) {
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,5 +1,9 @@
 /* exported tail */
 
+/* store an empty array into a variable called newArr
+Take the value of 1 and store it into a new variable called i.
+if i is less than the length of array, push array at i index into newArr
+*/
 function tail(array) {
   var newArr = [];
   for (var i = 1; i < array.length; i++) {
