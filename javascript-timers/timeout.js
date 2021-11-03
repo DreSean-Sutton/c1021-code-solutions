d8af5d4a33f6b39b0d5b8759188bcebfddefdb6d
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-03 11:19:36 -0700

    Sorry, i used arrow function as a test and meant to change it later

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -1,4 +1,6 @@
-setTimeout(() => {
+setTimeout(helloThere, 2000);
+
+function helloThere() {
   var $helloThere = document.querySelector('h1');
   $helloThere.textContent = 'Hello There';
-}, 2000);
+}
